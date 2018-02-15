ace67959d0c347cc69cc23455048bffb72b83833
jmpham <33913843+jmpham@users.noreply.github.com>
2018-02-14 23:24:26 -0600

    mailboxAPI is working. signup btn opens when email is valid

--- WebContent/WEB-INF/Login.js
+++ WebContent/WEB-INF/Login.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 	var input = $("#email");
+	var APIKey_mailboxlayer = "4e4a68c1aeb47e4de0c0d60a904749c5";
+	
 	//disable submit button until email is proven available
 	$('#signup').prop('disabled', true); //TO DISABLED $('.enableOnInput').prop('disabled', false); //TO ENABLE
 	
 	//check for email availability
 	$(".Availability").click(function(){
 		var email = input.val(); //val() returns the value of the attribute selected
+		var emailValid = false;
 		console.log("starting API with email: " + email);
 		$.ajax({
 			// need 'http://' in the beginning or else internet browser will complain. Need to use http protocol to talk
@@ -18,15 +21,38 @@ $(document).ready(function(){
 				$("#emailResult").html("email availability: " + result);
 				//enable submit button if email is available
 				if(result == true && $("#firstName").val() && $("#lastName").val() && $("#password").val() ){
-					$('#signup').prop('disabled', false);
+					console.log("ajax request done");
+					emailValid = true;
+					//$('#signup').prop('disabled', false);
 				}
-				else{
-					$('#signup').prop('disabled', true);
-				}
+				$.ajax({
+					url: "http://apilayer.net/api/check?access_key=" + APIKey_mailboxlayer + 
+						 "&email=" + email +
+						 "&smtp=1&format=1",
+					dataType: "json",
+					success: function(result){
+						console.log("Email SMTP result is: " + result["smtp_check"]);
+						console.log("emailValid: " + emailValid);
+						if(result["smtp_check"] == true && emailValid == true){
+							$('#signup').prop('disabled', false);
+						}
+						else{
+							$('#signup').prop('disabled', true);
+						}
+					},
+					Error: function(){
+						console.log('Error: 505 - bad connection - mailboxlayer API attempt')
+					}
+				});
 			},
 			Error: function(){
 	        console.log('Error: 505 - bad connection');
 	      }
 			});
+		
+		// API MAILBOX IS FINISHING BEFORE TOP REQUEST. NEED TO WAIT
+		
+		//Checking for email authenticity
+		
 	});
 })
